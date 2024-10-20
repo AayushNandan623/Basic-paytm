82c77f8886b69c7ef3bb609a745dcee501b2a78d
Aayush Nandan <121042190+AayushNandan623@users.noreply.github.com>
2024-10-20 15:19:41 +0530

    Added log out functionality

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,13 +1,24 @@
 import Line from "../components/Line";
 import ProfilePic from "../components/ProfilePic";
+import ButtonComp from "./ButtonComp";
+import { useNavigate } from "react-router-dom";
 function Appbar() {
+  const navigate = useNavigate();
   return (
     <>
       <div className=" flex flex-row justify-between items-center m-4">
         <div className=" font-bold text-2xl">Payments App</div>
         <div className=" flex justify items-center font-medium text-lg">
-          Hello, User<div className="p-1"></div>{" "}
+          Hello, User<div className="pr-3"></div>{" "}
           <ProfilePic char="U"></ProfilePic>
+          <div className="pr-3"></div>{" "}
+          <ButtonComp
+            label="Log Out"
+            onClick={() => {
+              localStorage.removeItem("token");
+              navigate("/signin");
+            }}
+          ></ButtonComp>
         </div>
       </div>
       <Line></Line>
